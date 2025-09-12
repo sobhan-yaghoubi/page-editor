57ea6cbec3b04c8430e8eb75708501fdb41099cc
sobhan yaghoubi <sobhan.yaghoubi.work@gmail.com>
2025-09-12 21:42:54 +0330

    fix: tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,51 @@
 import { defineConfig } from "tsup"
 
-export default defineConfig({
-  entry: ["src/index.ts", "src/editor.ts", "src/storefront.ts"],
-  format: ["esm", "cjs"],
-  splitting: true,
-  sourcemap: true,
-  clean: true,
-  dts: true,
-  banner: {
-    js: `
-      "use client";
-    `,
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    format: ["esm", "cjs"],
+    splitting: true,
+    sourcemap: true,
+    clean: false,
+    dts: true,
+    external: [
+      "react",
+      "react-dom",
+      "lucide-react",
+      "zustand",
+      "clsx",
+      "tailwind-merge",
+      "next/image",
+      "next/link",
+      "lodash",
+      "uuid",
+    ],
   },
-  external: ["react", "lucide-react", "zustand"],
-})
+
+  {
+    entry: ["src/editor.ts", "src/view.ts"],
+    format: ["esm", "cjs"],
+    splitting: true,
+    sourcemap: true,
+    clean: true,
+    dts: true,
+
+    esbuildOptions(options) {
+      options.banner = {
+        js: '"use client";',
+      }
+    },
+    external: [
+      "react",
+      "react-dom",
+      "lucide-react",
+      "zustand",
+      "clsx",
+      "tailwind-merge",
+      "next/image",
+      "next/link",
+      "lodash",
+      "uuid",
+    ],
+  },
+])
